bd59751548e9517e1f00b2dd8e6c936ebf8642c2
Prakash Paudel <prakash7m@gmail.com>
2016-10-22 13:26:08 +0545

    implemented login using passport

--- models/user.js
+++ models/user.js
@@ -1,4 +1,6 @@
 var mongoose = require("mongoose");
+var bcrypt = require('bcrypt-nodejs');
+var SALT_FACTOR = 10;
 var userSchema = mongoose.Schema({
     username: {
         type: String,
@@ -20,7 +22,40 @@ var userSchema = mongoose.Schema({
 userSchema.methods.name = function () {
     return this.displayName || this.username;
 }
+userSchema.methods.getBio = function () {
+    return this.bio || "Not available";
+}
 
-userSchema.methods.checkPassword = function () {
+userSchema.methods.checkPassword = function (toCheck, done) {
+    bcrypt.compare(toCheck, this.password, function (err, isMatch) {
+        done(err, isMatch);
+    })
+}
+
+userSchema.methods.isUnique = function (toCheck, done) {
     
 }
+
+userSchema.pre('save', function (done) {
+    var user = this;
+    if (!user.isModified("password")) {
+        return done();
+    }
+
+    bcrypt.genSalt(SALT_FACTOR, function (err, salt) {
+        if (err) {
+            return done(err);
+        }
+        bcrypt.hash(user.password, salt, function () {}, function (err, hashedPassword) {
+            if (err) {
+                return done(err);
+            }
+            user.password = hashedPassword;
+            done();
+        })
+    });
+});
+
+
+var User = mongoose.model("User", userSchema);
+module.exports = User;
