2e77bb382a7170c60194c8055b4840a72f0d977d
prakash <prakash7m@gmail.com>
2016-10-23 17:44:47 +0545

    fix for display name not shown correctly

--- setuppassport.js
+++ setuppassport.js
@@ -48,9 +48,9 @@ passport.use("facebook", new FacebookStrategy({
     callbackURL     : "http://localhost:9000/auth/facebook/callback",
     profileFields: ['id', 'first_name', 'last_name', 'picture', 'email', 'gender', 'link', 'locale', 'timezone', 'updated_time', 'verified']
 }, function (token, refreshToken, profile, done) {
-    console.log(profile)
     process.nextTick(function () {
         User.findOne({'facebook.id': profile.id}, function (err, user) {
+            
             if (err) {
                 return done(err);
             }
@@ -59,9 +59,9 @@ passport.use("facebook", new FacebookStrategy({
             } else {
                 var newUser = new User({
                     username: "facebook_" + profile.id + "_" + Math.random(100),
-                    displayName: profile.first_name + " " + profile.last_name,
-                    bio: profile.bio,
-                    password: "VerySecret",
+                    displayName: profile._json.first_name + " " + profile._json.last_name,
+                    bio: profile._json.bio,
+                    password: "VerySecret" + Math.random(1000), //just to validate mongo db
                     facebook: profile._json
                 });
                 newUser.save(function (err) {
