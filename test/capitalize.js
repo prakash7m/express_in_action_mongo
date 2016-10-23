1169b7c6aee7dacdc50bf56054fce084c2f29159
prakash <prakash7m@gmail.com>
2016-10-23 19:34:08 +0545

    Added sample test using mocha and chai

---
+++ test/capitalize.js
@@ -0,0 +1,19 @@
+var capitalize = require("../lib/capitalize")
+var chai = require("chai");
+var expect = chai.expect;
+
+
+describe("capitalize", function () {
+    it("Capitalizes words", function () {
+        expect(capitalize("express")).to.equal("Express");
+        expect(capitalize("hello")).to.equal("Hello");
+        expect(capitalize("h")).to.equal("H");
+        expect(capitalize("HH")).to.equal("Hh");
+        expect(capitalize("hH")).to.equal("Hh");
+    });
+
+    it("Capitalizes a sentence", function () {
+        expect(capitalize("this is test sentence")).to.equal("This is test sentence");
+        expect(capitalize("hello world")).to.equal("Hello world")
+    });
+});
